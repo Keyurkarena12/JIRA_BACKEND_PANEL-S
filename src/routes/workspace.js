747f19e66e120ff29f865b47fd6837beece39774
Keyurkarenai2 <karenakeyur20@gmail.com>
2026-05-05 12:52:28 +0530

    workspace in new changes

--- src/routes/workspace.js
+++ src/routes/workspace.js
@@ -1,17 +1,19 @@
     import express from "express";
-    import {acceptInvite, createWorkspace, getAcceptedInvitedUsers, getlistWorkspace, getWorkspaceById, getWorkspaceMembers, inviteMember } from "../controllers/Workspace/workspaceControllers.js";
-    import { auth } from "../middlewares/authmiddlewares.js";
-    import { isOwner } from "../middlewares/adminmiddleware.js";
+import {acceptInvite, createWorkspace, getAcceptedInvitedUsers, getlistWorkspace, getWorkspaceById, getWorkspaceMembers, inviteMember } from "../controllers/Workspace/workspaceControllers.js";
+import { auth } from "../middlewares/authmiddlewares.js";
+import { isOwner } from "../middlewares/adminmiddleware.js";
+import { checkPlanLimit } from "../middlewares/checkPlanLimits.js";
 
-    const router = express.Router();
+const router = express.Router();
 
-    router.post("/create", auth, createWorkspace);
+router.post("/create", auth, checkPlanLimit('workspace'), createWorkspace);
 
-    router.post("/add-member/:workspaceId",auth,isOwner,inviteMember)
+// router.post("/add-member/:workspaceId",auth,isOwner,inviteMember)
+router.post("/add-member/:workspaceId", auth, isOwner, checkPlanLimit('member'), inviteMember);
 
-    router.get("/accept-invite",acceptInvite)
+router.get("/accept-invite",acceptInvite)
 
-    router.get("/get-workspaces/:workspaceId",auth,getWorkspaceById)
+router.get("/get-workspaces/:workspaceId",auth,getWorkspaceById)
 
     router.get("/get-workspaces",auth,getlistWorkspace)
    
