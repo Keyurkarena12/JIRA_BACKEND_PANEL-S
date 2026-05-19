48a36dda18bb2721e48f3aefe394b48e99f715a8
Keyurkarenai2 <karenakeyur20@gmail.com>
2026-05-19 16:34:16 +0530

    directchat feature add

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -7,6 +7,7 @@ import {
   getUserChatRooms
 } from "../controllers/Chat/ChatController.js";
 
+import { getOrCreateDirectChat, getDirectConversations, searchUsers } from "../controllers/Chat/DirectChatController.js";
 const router = express.Router();
 
 router.use(auth);
@@ -16,5 +17,11 @@ router.get("/workspace/:workspaceId", getOrCreateWorkspaceChat);
 router.get("/project/:projectId", getOrCreateProjectChat);
 router.get("/messages/:roomId", getChatMessages);
 router.get("/rooms", getUserChatRooms);
+
+
+// direact one to one  user 
+router.get("/direct/conversations", getDirectConversations);
+router.get("/direct/search", searchUsers);
+router.get("/direct/:userId", getOrCreateDirectChat);
 
 export default router;
